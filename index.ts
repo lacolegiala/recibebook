c0d397f950f14ff0c7221cfa24be755b769713ee
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2022-02-25 21:34:02 +0200

    Throw an error if connection string env variable is not set

--- index.ts
+++ index.ts
@@ -8,43 +8,48 @@ import * as dotenv from "dotenv";
 
 dotenv.config()
 
+let connectionString: string
 if (process.env.PG_CONNECTION_STRING) {
-  const pool = createPool(process.env.PG_CONNECTION_STRING)
-
-  const allRecipes = pool.connect(async (connection) => {
-    const recipes = await connection.many(sql`SELECT * FROM recipe`)
-    return recipes
-  });
-  
-  const allIngredients = pool.connect(async (connection) => {
-    const ingredients = await connection.many(sql`SELECT * FROM ingredient`)
-    return ingredients
-  })
-  
-  const allMeals = pool.connect(async (connection) => {
-    const meals = await connection.many(sql`SELECT * FROM meal`)
-    return meals
-  })
-  
-  app.get('/', (req, res) => {
-    res.send('Welcome to your virtual recipe book!')
-  })
-  
-  app.get('/recipes', async (req, res) => {
-    const recipes = await allRecipes
-    res.send(recipes)
-  })
-  
-  app.get('/ingredients', async (req, res) => {
-    const ingredients = await allIngredients
-    res.send(ingredients)
-  })
-  
-  app.get('/meals', async (req, res) => {
-    const meals = await allMeals
-    res.send(meals)
-  })
+  connectionString = process.env.PG_CONNECTION_STRING
+} else {
+  throw new Error('Connection string environment variable is not set')
 }
+
+const pool = createPool(process.env.PG_CONNECTION_STRING)
+
+const allRecipes = pool.connect(async (connection) => {
+  const recipes = await connection.many(sql`SELECT * FROM recipe`)
+  return recipes
+});
+
+const allIngredients = pool.connect(async (connection) => {
+  const ingredients = await connection.many(sql`SELECT * FROM ingredient`)
+  return ingredients
+})
+
+const allMeals = pool.connect(async (connection) => {
+  const meals = await connection.many(sql`SELECT * FROM meal`)
+  return meals
+})
+
+app.get('/', (req, res) => {
+  res.send('Welcome to your virtual recipe book!')
+})
+
+app.get('/recipes', async (req, res) => {
+  const recipes = await allRecipes
+  res.send(recipes)
+})
+
+app.get('/ingredients', async (req, res) => {
+  const ingredients = await allIngredients
+  res.send(ingredients)
+})
+
+app.get('/meals', async (req, res) => {
+  const meals = await allMeals
+  res.send(meals)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
