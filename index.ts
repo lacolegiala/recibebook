2312f6bdae2ec308118e249a82ea1324c7e860b5
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2022-02-26 17:55:56 +0200

    Refactor simpler functions

--- index.ts
+++ index.ts
@@ -24,87 +24,100 @@ app.get('/', (req, res) => {
 })
 
 app.get('/recipes', async (req, res) => {
-  const allRecipes = pool.connect(async (connection) => {
-    return await connection.many(sql`SELECT * FROM recipe`)
-  });
-  res.send(await allRecipes)
+  const allRecipes = await pool.connect((connection) =>
+    connection.many(sql`SELECT * FROM recipe`)
+  );
+  res.send(allRecipes)
 })
 
 app.get('/ingredients', async (req, res) => {
-  const allIngredients = pool.connect(async (connection) => {
-    return await connection.many(sql`SELECT * FROM ingredient`)
-  })
-  res.send(await allIngredients)
+  const allIngredients = await pool.connect((connection) => 
+    connection.many(sql`SELECT * FROM ingredient`)
+  )
+  res.send(allIngredients)
 })
 
 app.get('/meals', async (req, res) => {
-  const allMeals = pool.connect(async (connection) => {
-    return await connection.many(sql`SELECT * FROM meal`)
-  })
-  res.send(await allMeals)
+  const allMeals = await pool.connect((connection) => 
+    connection.many(sql`SELECT * FROM meal`)
+  )
+  res.send(allMeals)
 })
 
 app.get('/recipes/:id', async (req, res) => {
   const id = req.params.id
-  const getRecipe = pool.connect(async (connection) => {
-    return await connection.many(sql`
+  const getRecipe = await pool.connect((connection) => 
+    connection.many(sql`
       SELECT * FROM recipe WHERE id=${id}
     `)
-  })
-  res.send(await getRecipe)
+  )
+  res.send(getRecipe)
 })
 
 app.get('/ingredients/:id', async (req, res) => {
   const id = req.params.id
-  const getIngredient = pool.connect(async (connection) => {
-    return await connection.many(sql`
+  const getIngredient = await pool.connect((connection) => 
+    connection.many(sql`
       SELECT * FROM ingredient WHERE id=${id}
     `)
-  })
-  res.send(await getIngredient)
+  )
+  res.send(getIngredient)
 })
 
 app.get('/meals/:id', async (req, res) => {
   const id = req.params.id
-  const getMeal = pool.connect(async (connection) => {
-    return await connection.many(sql`
+  const getMeal = await pool.connect((connection) => 
+    connection.many(sql`
       SELECT * FROM meal WHERE id=${id}
     `)
-  })
-  res.send(await getMeal)
+  )
+  res.send(getMeal)
+})
+
+app.get('/recipes/:id/details', async (req, res) => {
+  const id = req.params.id
+  const getDetails = await pool.connect((connection) => 
+    connection.many(sql`
+      SELECT ingredient.name FROM ingredient
+      JOIN recipeingredient ON recipeingredient.ingredientid = ingredient.id
+      JOIN recipe ON recipe.id = recipeingredient.recipeid
+      WHERE recipe.id=${id};
+    `)
+  )
+  res.send(getDetails)
 })
 
 app.post('/recipes', async (req, res) => {
   const recipe: {name: string, method: string, servings: number} = req.body
-  const addRecipe = pool.connect(async (connection) => {
-    return await connection.query(sql`
+  const addRecipe = await pool.connect((connection) => 
+    connection.query(sql`
       INSERT INTO recipe (name, method, servings)
       VALUES (${recipe.name}, ${recipe.method}, ${recipe.servings})
     `)
-  })
-  res.send(await addRecipe)
+  )
+  res.send(addRecipe)
 })
 
 app.post('/ingredients', async (req, res) => {
   const ingredient: {name: string, foodGroup: string} = req.body
-  const addIngredient = pool.connect(async (connection) => {
-    return await connection.query(sql`
+  const addIngredient = await pool.connect((connection) =>
+    connection.query(sql`
       INSERT INTO ingredient (name, foodGroup)
       VALUES (${ingredient.name}, ${ingredient.foodGroup})
     `)
-  })
-  res.send(await addIngredient)
+  )
+  res.send(addIngredient)
 })
 
 app.post('/meals', async (req, res) => {
   const meal: {name: string} = req.body
-  const addMeal = pool.connect(async (connection) => {
-    return await connection.query(sql`
+  const addMeal = await pool.connect((connection) =>
+    connection.query(sql`
       INSERT INTO meal (name)
       VALUES (${meal.name})
     `)
-  })
-  res.send(await addMeal)
+  )
+  res.send(addMeal)
 })
 
 app.listen(port, () => {
