abcb979189617058abb7bf0d4568038bf09d2d56
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2022-02-27 21:14:51 +0200

    Set port value to process env port

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 import { createPool, sql } from "slonik";
 import * as dotenv from "dotenv";
 import { body, validationResult } from "express-validator";
